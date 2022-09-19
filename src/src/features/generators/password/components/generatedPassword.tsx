0e575e774e5ac47e43f4c8f0289cce25396fd010
oyama <52624121+yoyama92@users.noreply.github.com>
2022-09-19 12:16:02 +0900

    生成パスワード表示コンポーネントの実装

--- src/src/features/generators/password/components/generatedPassword.tsx
+++ src/src/features/generators/password/components/generatedPassword.tsx
@@ -1,10 +1,10 @@
 import {
+  Box,
   Card,
   FormControl,
   Input,
   styled,
   Typography,
-  useTheme,
 } from '@mui/material';
 
 const Style = styled('div')(({ theme }) => ({
@@ -16,36 +16,33 @@ type PasswordProps = {
 };
 
 const GeneratedPassword = ({ values }: PasswordProps) => {
-  const theme = useTheme();
-  const value = values.join('\n');
   return (
     <Style>
       <Typography variant="subtitle1">Generated Password</Typography>
-      <Card sx={{ height: `calc(100% -  ${theme.spacing(2)})` }}>
-        <FormControl
-          sx={{
-            flexGrow: 1,
-            m: 1,
-            width: '100%',
-            height: '100%',
-          }}
-          size="small"
-        >
-          <Input
-            multiline
-            readOnly
-            disableUnderline
-            sx={{
-              mx: 1,
-              overflow: 'auto',
-              height: '100%',
-              width: '100%',
-              alignItems: 'start',
-            }}
-            value={value}
-          />
-        </FormControl>
-      </Card>
+      {values.map((value, index) => {
+        return (
+          <Box>
+            <Card
+              key={`${index}-${value}`}
+              sx={{
+                m: 1,
+                px: 1,
+                py: 0.5,
+                width: '100%',
+              }}
+            >
+              <FormControl
+                sx={{
+                  width: '100%',
+                }}
+                size="small"
+              >
+                <Input readOnly disableUnderline value={value} />
+              </FormControl>
+            </Card>
+          </Box>
+        );
+      })}
     </Style>
   );
 };
